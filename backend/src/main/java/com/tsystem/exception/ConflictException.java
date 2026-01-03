6f1f709d0183901a117e9878db471f4b8a831b51
Viacheslav Dmytrenko <75087250+20Viacheslav20@users.noreply.github.com>
2026-01-04 00:16:16 +0100

    remove unused 2

--- backend/src/main/java/com/tsystem/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.tsystem.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String m) {
-        super(m);
-    }
-}
--- backend/src/main/java/com/tsystem/exception/UnprocessableException.java
+++
@@ -1,7 +0,0 @@
-package com.tsystem.exception;
-
-public class UnprocessableException extends RuntimeException {
-    public UnprocessableException(String message) {
-        super(message);
-    }
-}
