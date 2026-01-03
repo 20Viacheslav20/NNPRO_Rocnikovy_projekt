6ceaa77aff47ea5679e8bb17227d97beff4b2997
Viacheslav Dmytrenko <75087250+20Viacheslav20@users.noreply.github.com>
2026-01-04 00:15:21 +0100

    remove unused

--- backend/src/main/java/com/tsystem/web/ErrorHandling.java
+++ backend/src/main/java/com/tsystem/web/ErrorHandling.java
@@ -7,5 +7,4 @@ import org.springframework.web.bind.annotation.*;
 @RestControllerAdvice
 public class ErrorHandling {
     @ResponseStatus(HttpStatus.NOT_FOUND) @ExceptionHandler(NotFoundException.class) String nf(NotFoundException e){return e.getMessage();}
-    @ResponseStatus(HttpStatus.FORBIDDEN) @ExceptionHandler(ForbiddenException.class) String fb(ForbiddenException e){return e.getMessage();}
 }
